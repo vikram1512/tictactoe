c678509a71593bec085216c57e911b0ae39d3cc1
vikram <vikramvarmakunaparaju@gmail.com>
2020-12-27 16:50:00 -0500

    Added styles

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Square = props => {
-  return <button type="button">{props.value}</button>;
+const Square = ({ value }) => {
+  return (
+    <button type="button" className="square">
+      {value}
+    </button>
+  );
 };
 
 export default Square;
