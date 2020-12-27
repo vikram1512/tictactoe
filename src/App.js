2b4b1ec482a4d497fe96725c2ad5a88cf76e2e72
vikram <vikramvarmakunaparaju@gmail.com>
2020-12-27 13:45:10 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to React Parcel Micro App!!</h1>
+      <p>Hello World</p>
+      <small>Hello this is Vikram</small>
+    </div>
+  );
+};
+
+export default App;
